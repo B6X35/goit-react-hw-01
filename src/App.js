4c32866364a967f654ce72af261fffe87a552ea1
B6X35 <d.b.palamarchuk@gmail.com>
2021-12-07 12:47:28 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import './style/App.css';
 import Profile from './components/Profile/Profile';
-import user from './data/user.json';
 import Statistics from './components/Statistics/Statistics';
-import data from './data/data.json';
 import FriendList from './components/FriendLIst/FriendList';
-import friends from './data/friends.json';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './data/transactions.json';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Profile  
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
+        username={data.user.username}
+        tag={data.user.tag}
+        location={data.user.location}
+        avatar={data.user.avatar}
+        stats={data.user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Statistics title="Upload stats" stats={data.stat} />
+      <Statistics stats={data.stat} />
+      <FriendList friends={data.friends} />
+      <TransactionHistory items={data.transactions} />
     </div>
   );
 }
---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+import user from './user.json';
+import stat from './data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+const data = {
+    user,
+    stat,
+    friends,
+    transactions
+}
+
+export default data
